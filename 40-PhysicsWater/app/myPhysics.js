4dd02ea96ba65057f692a128ef03e2f80fa5bb09
Robert Woodley <blwoodley@yahoo.com>
2015-02-20 22:49:05 -0600

    made grid move

--- 40-PhysicsWater/app/myPhysics.js
+++ 40-PhysicsWater/app/myPhysics.js
@@ -38,7 +38,7 @@ physicsBox = function(scene, x, y, l, w, h, t) {
 }
 var _balls = [];
 var 
-shootBall = function(waterY, scene, x, y, width, w, dx, dy, material) {
+shootBall = function(waterY, scene, x, y, width, w, dx, dy, material, applyForce) {
     var ball;
     for (var i = 0; i < _balls.length; i++) {   // see if we can reuse a ball
         if (_balls[i].position.y < waterY) {
@@ -50,7 +50,7 @@ shootBall = function(waterY, scene, x, y, width, w, dx, dy, material) {
     if (ball == undefined) {
         console.log("New....");
         ball = new Physijs.SphereMesh(
-                 new THREE.SphereGeometry(width*5, 20),
+                 new THREE.SphereGeometry(5*width, 20),
                  Physijs.createMaterial(
                          //new THREE.MeshPhongMaterial(
                          //        {   color: 0x00f,
@@ -65,24 +65,46 @@ shootBall = function(waterY, scene, x, y, width, w, dx, dy, material) {
          );
         _balls.push(ball);
     }
-    ball.position.set(
+    //ball.scale.x = 5*width;
+    //ball.scale.y = 5*width;
+    //ball.scale.z = 5*width;
+    ball.position.set(   //x,y,0);
             x + Math.random() * w - w/2,
             y + Math.random() * w - w/2,
             Math.random() * w - w/2
     );
     scene.add(ball);
-    var force_vector = new THREE.Vector3(dx, dy, 0);
-    ball.applyCentralImpulse(force_vector);
+    if (applyForce) {
+        var force_vector = new THREE.Vector3(dx, dy, 0);
+        ball.applyCentralImpulse(force_vector);
+    }
 }
 function doDroplets(waterY, radians, tick, scene, material) {
-  if (tick%50 > 20) return;
-  if (tick%5 != 0) return;
-  var rad = (radians)%Math.PI/8;
+    //radians -= Math.PI/32;
+  var rad = Math.abs(radians)%(Math.PI/4);
+  //if (Math.abs(rad) > Math.PI/8 || Math.abs(rad) < Math.PI/16) return; // bursts
+  //if (Math.abs(rad) > 7*Math.PI/32) return;
+  if (tick%2 != 0) return;      // don't make too many droples!
+  //console.log(radians, rad);
   var radius = 10;
-  var rotation = Math.PI/2+Math.PI/4+Math.PI/8;
+  var rotation = Math.PI/2+Math.PI/8-Math.PI/8;
   var x = radius*Math.cos(rad+rotation);
   var y = radius*Math.sin(rad+rotation);
-  for (var i = 0; i < 3; i++) {
-    shootBall(waterY, scene, x, y, .02, 1.5, 75, 125, material);
+
+  // arc of bubbles being flung up...
+  for (var i = 0; i < 1; i++) {
+    var dropletDiameter = 0.03  + Math.random() * .02;
+    shootBall(waterY, scene, x, y, dropletDiameter, 0.05, -75*x/radius, 125*y/radius, material, true);
   }
+
+  // drips falling down.
+  rotation += Math.PI/2;
+  var x = radius*Math.cos(rad+rotation);
+  var y = radius*Math.sin(rad+rotation);
+  for (var i = 0; i < 1; i++) {
+    var dropletDiameter = 0.01  + Math.random() * .01;
+    shootBall(waterY, scene, x, y, dropletDiameter, 0.05, 0,0, material, false);
+  }
+
+
 }
