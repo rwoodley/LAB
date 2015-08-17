ea643a34b50f601ee346e46142f32839a0094822
Robert Woodley <blwoodley@yahoo.com>
2015-08-16 23:43:58 -0500

    cleaned up physics code. moved everything into Planets class

--- 55-Durango/app/MainScene.js
+++ 55-Durango/app/MainScene.js
@@ -38,96 +38,17 @@ var MainScene = function(containingDiv, canvas, camera, objectCache) {
     skyBox.rotation.x = Math.PI/4;
     _that._scene.add( skyBox );
 
-    // The pluto/charon system references this center point. no absolute positions.
-    var _plutoCenter = new Cart3(AUm * 40, 0, 0);
-
-    var radiusPluto = 736/AUm;
-    var plutoGeometry = new THREE.SphereGeometry( radiusPluto * 2, 32, 32 );
-    plutoTexture = THREE.ImageUtils.loadTexture('textures/ganymede.jpg');
-    var plutoMaterial = new THREE.MeshBasicMaterial({map: plutoTexture, side: THREE.DoubleSide });
-    var plutoMesh = new THREE.Mesh( plutoGeometry, plutoMaterial );
-    plutoMesh.position.set(_plutoCenter.x, _plutoCenter.y, _plutoCenter.z);
-    _that._scene.add(plutoMesh);
-    objectCache.plutoMesh = plutoMesh;
-
-    var radiusNeptune = 1236/AUm;
-    var neptuneGeometry = new THREE.SphereGeometry( radiusPluto * 2, 32, 32 );
-    neptuneTexture = THREE.ImageUtils.loadTexture('textures/pluto.jpg');
-    var neptuneMaterial = new THREE.MeshBasicMaterial({map: neptuneTexture, side: THREE.DoubleSide });
-    var neptuneMesh = new THREE.Mesh( neptuneGeometry, neptuneMaterial );
-    neptuneMesh.position.set(_plutoCenter.x -  AUm * .5, _plutoCenter.y, _plutoCenter.z);
-    _that._scene.add(neptuneMesh);
-    objectCache.neptuneMesh = neptuneMesh;
-    
-    _that._physics = new engine();
-    // positions, everything is in AUm/Divisor
-    var ff = AUDivisor/AUm;      // fudge factor
-    //_that._physics.addPlanet({
-    //    'name': 'pluto',
-    //    'mass': 1e25,   // in units of earth. should really be 333,000
-    //    'radius': 2440000,
-    //    'pos': new Cart3(-57900000000, 0, 0),
-    //});
-    //_that._physics.addPlanet({
-    //    'name': 'neptune',
-    //    'mass': 1.589e30,   // in units of earth. should really be 333,000
-    //    'radius': 2440000,
-    //    'pos': new Cart3(0, 0, 0),
-    //});
-    _that._physics.addPlanet({
-        'name': 'charon',
-        'mass': 1.52e21,   // in units of earth. should really be 333,000
-        'radius': 1000 * 1000,
-        'pos': new Cart3(19640 * 1000, 0, 0),
-        'vel': 210
-    });
-    _that._physics.addPlanet({
-        'name': 'pluto',
-        'mass': 1.27e22,   // in units of earth. should really be 333,000
-        'radius': 1137 * 1000,
-        'pos': new Cart3(0, 0, 0),
-        'vel': -26
-    });
-
-    /*
-     *pluto mass: 1.30900 x 10^22 kg
-     *distance: 19,640 km
-     *charon mass: 1.90e+21kg
-     *code:
-     *  radius is in m
-     *  mass is in kg
-     */
-    //this.convert = function(inpos) {
-    //    return new Cart3(
-    //        inpos.x
-    //        );
-    //}
-
     var spotLight = new THREE.SpotLight( 0xaaaa00 );
     spotLight.position.set( 0, 0, 0 );
     _that._scene.add(spotLight);
 
+    addPlanets(_that._scene, objectCache);
+    
     _that._renderer.render( _that._scene, _that._camera );
     _that._frame = 0;
     this.render = function() {
         _that._frame += .001;
-        _that._physics.updateObjects(_that._frame);
-        var ff = 3e05;
-        //for (var i = 0; i < 2; i++)
-        //var i = 0;
-        //    console.log(_that._physics.planets[i].pos.x/ff,_that._physics.planets[i].pos.y/ff,(_that._physics.planets[i].pos.z/ff).toFixed(2));
-        var ff2 = ff/900;
-        objectCache.plutoMesh.position.set(
-            _that._physics.planets[1].pos.x/ff + _plutoCenter.x,
-            _that._physics.planets[1].pos.y/ff + _plutoCenter.y,
-            _that._physics.planets[1].pos.z/ff + _plutoCenter.z
-            );
-        objectCache.neptuneMesh.position.set(
-            _that._physics.planets[0].pos.x/ff + _plutoCenter.x,
-            _that._physics.planets[0].pos.y/ff + _plutoCenter.y,
-            _that._physics.planets[0].pos.z/ff + _plutoCenter.z
-            );
-        //_that._controls.update( _that._clock.getDelta() );
+        updatePlanetPhysics(_that._frame);
         _that._renderer.render( _that._scene, _that._camera );
 
     }
--- 55-Durango/app/engine.js
+++ 55-Durango/app/engine.js
@@ -1,31 +1,18 @@
-var engine = function() {
+var engine = function(planets) {
     var _that = this;
     this.G = 6.6742e-11; // universal gravitational constant
 
-    this.planets = [];
-    //this.planets.push({
-    //    'name': 'sun',
-    //    'mass': 3000,   // in units of earth. should really be 333,000
-    //    'position': new Cart3(0,0,0)        
-    //});
-    this.addPlanet = function(obj) {
-        var vel = new Cart3(0,0,obj.vel);
-
-        var ob = new OrbitBody(obj.name, obj.radius, obj.pos, vel, obj.mass, undefined);
-        this.planets.push(ob);
-    }
-    var _firstTime = false;
+    this.planets = planets;
     this.updateOrbit = function(pa, pb, dt, lambda) {
-        if (_firstTime) {
-            var radius = pa.pos.sub(pb.pos).x;
-            var v = Math.sqrt(Math.abs(_that.G * pb.mass/radius));
-            v *= Math.sign(radius);
-            console.log("v, radius = " + v + "," + radius);
-            pa.vel = new Cart3(0,0,v);
-        }
+        //var radius = pa.position.sub(pb.position).x;
+        //var v = Math.sqrt(Math.abs(_that.G * pb.mass/radius));
+        //v *= Math.sign(radius);
+        //console.log("v, radius = " + v + "," + radius);
+        //pa.velocity = new Cart3(0,0,v);
+
         // pa rotates around pb
         // vel, pos for pa is updated.
-        // inputs: pa.pos, pb.pos, pa.vel, pb.mass
+        // inputs: pa.position, pb.position, pa.velocity, pb.mass
       // don't compute self-gravitation
       if (pa != undefined && pb != undefined && pa != pb) {
         // this trig-free method does this:
@@ -34,35 +21,14 @@ var engine = function() {
         // including an optional dark energy constant (lambda)
         
         // compute radius of separation
-        var radius = pa.pos.sub(pb.pos);
+        var radius = pa.position.sub(pb.position);
         // update velocity with gravitational acceleration
-        pa.vel.addTo(radius.mult(dt * (lambda - _that.G * pb.mass * radius.invSumCube())));
-        if (_firstTime)
-            console.log(radius.mult(dt * (lambda - _that.G * pb.mass * radius.invSumCube())));
+        pa.velocity.addTo(radius.mult(dt * (lambda - _that.G * pb.mass * radius.invSumCube())));
+
         // update position with velocity
-        pa.pos.addTo(pa.vel.mult(dt));
+        pa.position.addTo(pa.velocity.mult(dt));
       }
     }
-    this.updateObjects = function(dt) {
-        dt = 1150;
-      // compute gravitation only wrt the sun, not wrt all other bodies
-      //for (var i = 0;i < _that.planets.length;i++) {
-          //for (var j = 0;j < _that.planets.length;j++) {
-                _that.updateOrbit(_that.planets[0], _that.planets[1], dt, 0);
-                _that.updateOrbit(_that.planets[1], _that.planets[0], dt, 0);
-        _firstTime = false;
-          //}
-      //}
-    }
-}
-
-OrbitBody = function(name, radius, pos, vel, mass, color) {
-  this.name = name; // string
-  this.radius = radius; // scalar
-  this.mass = mass; // scalar
-  this.pos = pos; // Cart3 3D vector
-  this.vel = vel; // Cart3 3D vector
-  this.color = color; // string
 }
 
 function Cart3(x,y,z) {
