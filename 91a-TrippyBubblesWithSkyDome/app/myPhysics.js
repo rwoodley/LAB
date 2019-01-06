b8e8ad4b32d06aee0a26affe8f4fcc447d3a0fc2
Robert Woodley <blwoodley@yahoo.com>
2019-01-06 09:42:36 -0600

    adding 91a-TrippyBubblesWithSkyDome

---
+++ 91a-TrippyBubblesWithSkyDome/app/myPhysics.js
@@ -0,0 +1,241 @@
+var _nextRandom = [];
+_nextRandom[0] = 0.5202335405742939;
+_nextRandom[1] = 0.5044463224477835;
+_nextRandom[2] = 0.3107165937394736;
+_nextRandom[3] = 0.6073999788126749;
+_nextRandom[4] = 0.20799813715887505;
+_nextRandom[5] = 0.4229239128637141;
+_nextRandom[6] = 0.2374016980705309;
+_nextRandom[7] = 0.5418023729616679;
+_nextRandom[8] = 0.5413397816864964;
+_nextRandom[9] = 0.6822867226863387;
+_nextRandom[10] = 0.31142967348412376;
+_nextRandom[11] = 0.47160527510141526;
+_nextRandom[12] = 0.3529978639404272;
+_nextRandom[13] = 0.23761875492214846;
+_nextRandom[14] = 0.4109997116124424;
+_nextRandom[15] = 0.028156802489145205;
+_nextRandom[16] = 0.4173076390757051;
+_nextRandom[17] = 0.45709382711867064;
+_nextRandom[18] = 0.41776701656552206;
+_nextRandom[19] = 0.4452509275747776;
+_nextRandom[20] = 0.9793200403230273;
+_nextRandom[21] = 0.4343749878296459;
+_nextRandom[22] = 0.6606428885623847;
+_nextRandom[23] = 0.9560417811501201;
+_nextRandom[24] = 0.41462920317289864;
+_nextRandom[25] = 0.5165925811958139;
+_nextRandom[26] = 0.44359620074032113;
+_nextRandom[27] = 0.8261676997340284;
+_nextRandom[28] = 0.22991369837411635;
+_nextRandom[29] = 0.8907003734112913;
+_nextRandom[30] = 0.9272787037680013;
+_nextRandom[31] = 0.9901764566718143;
+_nextRandom[32] = 0.1386979693751551;
+_nextRandom[33] = 0.4969152556547165;
+_nextRandom[34] = 0.692361682462199;
+_nextRandom[35] = 0.9215947882422031;
+_nextRandom[36] = 0.7932842589190712;
+_nextRandom[37] = 0.018930746856003156;
+_nextRandom[38] = 0.1969564170529552;
+_nextRandom[39] = 0.4801384602868981;
+_nextRandom[40] = 0.2527258530226877;
+_nextRandom[41] = 0.8835904262541145;
+_nextRandom[42] = 0.950034168965624;
+_nextRandom[43] = 0.3855630549162343;
+_nextRandom[44] = 0.9746734465908624;
+_nextRandom[45] = 0.5474843548669508;
+_nextRandom[46] = 0.9385271066202798;
+_nextRandom[47] = 0.38198161619127524;
+_nextRandom[48] = 0.6394220070195515;
+_nextRandom[49] = 0.49205347203357075;
+_nextRandom[50] = 0.3408863724016067;
+_nextRandom[51] = 0.8149804724174132;
+_nextRandom[52] = 0.14012458777935888;
+_nextRandom[53] = 0.8060136233657957;
+_nextRandom[54] = 0.26397516714517;
+_nextRandom[55] = 0.6943290825799502;
+_nextRandom[56] = 0.2854242186490581;
+_nextRandom[57] = 0.2994756847588347;
+_nextRandom[58] = 0.4025680477025684;
+_nextRandom[59] = 0.14650488719983268;
+_nextRandom[60] = 0.6109147531600221;
+_nextRandom[61] = 0.8943907907997306;
+_nextRandom[62] = 0.9352342470677706;
+_nextRandom[63] = 0.9391654699260457;
+_nextRandom[64] = 0.08775396743307629;
+_nextRandom[65] = 0.9348655006008395;
+_nextRandom[66] = 0.05312082200979629;
+_nextRandom[67] = 0.9061305293262965;
+_nextRandom[68] = 0.34975069614665943;
+_nextRandom[69] = 0.5780432618544411;
+_nextRandom[70] = 0.18685387215143145;
+_nextRandom[71] = 0.3519852948934412;
+_nextRandom[72] = 0.946236050586015;
+_nextRandom[73] = 0.3132100402693734;
+_nextRandom[74] = 0.33835360147164684;
+_nextRandom[75] = 0.850321798572399;
+_nextRandom[76] = 0.5972663952031672;
+_nextRandom[77] = 0.6027132270397146;
+_nextRandom[78] = 0.6738129088586065;
+_nextRandom[79] = 0.2294081117482718;
+_nextRandom[80] = 0.1528678444448497;
+_nextRandom[81] = 0.569116851789635;
+_nextRandom[82] = 0.5937138257612908;
+_nextRandom[83] = 0.506506262239959;
+_nextRandom[84] = 0.6098393574730103;
+_nextRandom[85] = 0.28755599532144793;
+_nextRandom[86] = 0.4397067235245642;
+_nextRandom[87] = 0.5164424941751304;
+_nextRandom[88] = 0.7117679626392772;
+_nextRandom[89] = 0.40893243643204324;
+_nextRandom[90] = 0.12304167403157384;
+_nextRandom[91] = 0.8692047997194194;
+_nextRandom[92] = 0.7745558881767043;
+_nextRandom[93] = 0.1405034016382276;
+_nextRandom[94] = 0.028297314488307634;
+_nextRandom[95] = 0.1114535033292543;
+_nextRandom[96] = 0.11479293789563383;
+_nextRandom[97] = 0.3312399532065857;
+_nextRandom[98] = 0.4183491777752524;
+_nextRandom[99] = 0.18801900916873482;
+_nextRandom[100] = 0.15335483508483794;
+_nextRandom[101] = 0.20910774154557132;
+_nextRandom[102] = 0.562377565311124;
+_nextRandom[103] = 0.007583285418453656;
+_nextRandom[104] = 0.6212122639565576;
+_nextRandom[105] = 0.39056969825596877;
+_nextRandom[106] = 0.9612099744750184;
+_nextRandom[107] = 0.7921134433132442;
+_nextRandom[108] = 0.33077412997158295;
+_nextRandom[109] = 0.5479226388724499;
+_nextRandom[110] = 0.42417548761489865;
+_nextRandom[111] = 0.7445055375869649;
+_nextRandom[112] = 0.37771069407924096;
+_nextRandom[113] = 0.9806944953157326;
+_nextRandom[114] = 0.5675328651219718;
+_nextRandom[115] = 0.029335701637341005;
+_nextRandom[116] = 0.7669664423117692;
+_nextRandom[117] = 0.6530754448839591;
+_nextRandom[118] = 0.4579414632185115;
+_nextRandom[119] = 0.8943072672816994;
+_nextRandom[120] = 0.09843834355627124;
+_nextRandom[121] = 0.7091216281818191;
+_nextRandom[122] = 0.15273498905116933;
+_nextRandom[123] = 0.8042375016884531;
+_nextRandom[124] = 0.05727773577537376;
+_nextRandom[125] = 0.5195993665381984;
+_nextRandom[126] = 0.14059432677838002;
+_nextRandom[127] = 0.48556326519898496;
+_nextRandom[128] = 0.20817175159851908;
+_nextRandom[129] = 0.26115911061726504;
+_nextRandom[130] = 0.859928517547194;
+_nextRandom[131] = 0.5992908499254914;
+_nextRandom[132] = 0.9310625589518773;
+_nextRandom[133] = 0.9668413496577759;
+_nextRandom[134] = 0.6423885609464091;
+_nextRandom[135] = 0.7639339758704513;
+_nextRandom[136] = 0.9518222434088079;
+_nextRandom[137] = 0.6618499926076331;
+_nextRandom[138] = 0.6583318134923408;
+_nextRandom[139] = 0.9335641814803142;
+_nextRandom[140] = 0.9015032533272529;
+_nextRandom[141] = 0.6138251963913324;
+_nextRandom[142] = 0.1330516863647262;
+_nextRandom[143] = 0.5072556765048017;
+_nextRandom[144] = 0.35643804435515736;
+_nextRandom[145] = 0.9272679327627602;
+_nextRandom[146] = 0.17784090033025324;
+_nextRandom[147] = 0.9964545688244926;
+_nextRandom[148] = 0.8578926314105275;
+_nextRandom[149] = 0.29568095178711884;
+_nextRandom[150] = 0.9851174409603392;
+_nextRandom[151] = 0.5083756669038741;
+_nextRandom[152] = 0.00958687204065245;
+_nextRandom[153] = 0.15133212153799946;
+_nextRandom[154] = 0.4462350615253383;
+_nextRandom[155] = 0.6050389213636325;
+var _groundMaterial;
+function makeMesh(w,h,d,x,y,z) {
+    var geo = new THREE.BoxGeometry(w,h,d);
+    var mesh = new Physijs.BoxMesh(geo, _groundMaterial, 0);
+    mesh.position.x = x; mesh.position.y = y; mesh.position.z = z;
+    return mesh;
+}
+physicsBox = function(x, y, l, w, h, t) {
+    //if (_groundMaterial == undefined) {
+        _groundMaterial = Physijs.createMaterial(
+                new THREE.MeshLambertMaterial({opacity:0.02, transparent: true }),
+                .9, // high friction
+                .6 // low restitution
+        );
+    //}
+
+    var ground = makeMesh(w, t, l, x, y, 0);
+    ground.receiveShadow = true;
+
+    var borderLeft = makeMesh(t, h, l, x-w/2+t/2, h/2+t/2, y);
+    ground.add(borderLeft);
+
+    var borderRight = makeMesh(t, h, l, x+w/2-t/2, h/2+t/2, y);
+    ground.add(borderRight);
+
+    var borderBottom = makeMesh(w-t-t, h, t, x, h/2+t/2, y-l/2+t/2);
+    ground.add(borderBottom);
+
+    var borderTop = makeMesh(w-t-t, h, t, x, h/2+t/2, y+l/2-t/2);
+    ground.add(borderTop);
+    //
+    return ground;
+}
+addSpheres = function (scene, scale, n, x, y, w) {
+    var colorSphere = scale(myRandom()).hex();
+    for (var i = 0; i < n; i++) {
+        box = new Physijs.SphereMesh(
+                new THREE.SphereGeometry(.4, 20),
+                Physijs.createMaterial(
+                        new THREE.MeshPhongMaterial(
+                                {   color: colorSphere,
+                                    opacity: 0.2,
+                                    transparent: true
+                                }),
+                        .4, // controls.sphereFriction,
+                        .4  // controls.sphereRestitution
+                ),
+                10
+        );
+        box.position.set(
+                x + myRandom() * w - w/2,
+                y,
+                myRandom() * w - w/2
+        );
+        scene.add(box);
+        var force_vector = new THREE.Vector3(50, 50, 0);
+        box.applyCentralImpulse(force_vector);
+    }
+    ;
+};
+var _nextRandomIndex = 0;
+function myRandom() {
+    var x=_nextRandom[_nextRandomIndex];
+    // console.log("_nextRandom[" + _nextRandom + "] = " + x + ";")
+    _nextRandomIndex++;
+    _nextRandomIndex = _nextRandomIndex%_nextRandom.length;
+    return x;
+    // return _myRandoms[_nextRandom++];
+}
+shootBall = function(scene, scale, x, y, width, w, dx, dy, material) {
+    var colorSphere = scale(myRandom()).hex();
+    box = new THREE.Mesh(
+             new THREE.SphereGeometry(width*5, 20, 64, 64),
+                     material
+             );
+     ww = w * 1.5;
+     box.position.set(
+             x + myRandom() * ww - ww/2,
+             y + myRandom() * w - w/2,
+             myRandom() * w - w/2
+     );
+     scene.add(box);
+     var force_vector = new THREE.Vector3(dx, dy, 0);
+}
